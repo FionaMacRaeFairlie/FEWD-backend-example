deb07b33dbbc94b791ca4aa257c66d2409cefc3a
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-11-14 09:43:35 +0000

    backend lab example

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const public = path.join(__dirname,'public');
+const path = require("path");
+const public = path.join(__dirname, "public");
 app.use(express.static(public));
 
-app.get('/', function(req, res) {
-    res.send('Hello! Welcome to my application.');
-    })
-    
-app.get('/guestbook', function(req, res) {
-      res.send('<h1>Guestbook Messages</h1>');
-        })
+app.get("/", function (req, res) {
+  res.send("Hello! Welcome to my application.");
+});
 
-app.get('/about', function(req, res) {
-res.sendFile(path.join(public, 'about.html'));
-})
+app.get("/guestbook", function (req, res) {
+  res.send("<h1>Guestbook Messages</h1>");
+});
 
-app.get('/data', function(req, res) {
-    res.json([{name:'s'}]);
-    })
+app.get("/about", function (req, res) {
+  res.sendFile(path.join(public, "about.html"));
+});
 
-app.use(function(req, res) {
-res.status(404);
-res.send('Oops! We didn\'t find what you are looking for.');
-})
-app.listen(3000, () => {
-    console.log('Server started on port 3000. Ctrl^c to quit.');
-    })
+app.get("/data", function (req, res) {
+  res.json([{ name: "s" }]);
+});
+
+app.get("/xxx", function (req, res) {
+  res.redirect("/");
+});
+
+
+app.use(function(err, req, res, next) {
+    res.status(500);
+    res.type('text/plain');
+    res.send('Internal Server Error.');
+});
+
+app.use(function (req, res) {
+  res.status(404);
+  res.send("Oops! We didn't find what you are looking for.");
+});
+
+app.listen(3001, () => {
+  console.log("Server started on port 3001. Ctrl^c to quit.");
+});
